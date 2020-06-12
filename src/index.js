661b02f135daadebccf9d18970a28af7d9fbeb21
fanutri1@gmail.com <maurojimenez@MacBook-Pro-de-Mauro.local>
2020-06-11 22:20:58 -0400

    se agrega funcionalidad de validacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+let creditCardNumber;
+
+
+const move = (close,open) => {
+    document.getElementById(close).style.display= 'none';
+    document.getElementById(open).style.display= 'block';
+   
+}
+
+const btnValidation  = document.getElementById('btnValidation');
+
+btnValidation .addEventListener('click', () => {
+    creditCardNumber = document.getElementById('creditCardNumber').value; 
+console.log(creditCardNumber);
+
+let textUno = document.getElementById('textoUno');
+ 
+ if (creditCardNumber.length > 0 ){
+    move("welcome", "validationResult");
+    isValid(creditCardNumber);
+
+    
+ } else {
+    textUno.innerHTML = 'Por favor, ingrese el numero de la tarjeta';
+     
+ }
+ 
+});
+
+
+const isValid = (creditCardNumber) =>{
+    let list = creditCardNumber.split('');
+     console.log(list);
+     
+}
+
